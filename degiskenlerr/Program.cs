1bcd2028ba028754942e641f0b6ac03dea388dc4
Harun Azun <harunazun@gmail.com>
2025-05-20 10:52:11 +0300

    Add project files.

---
+++ degiskenlerr/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace degiskenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string isim = "Ali";
+            string soyisim = "Yılmaz";
+
+            int yasim = 20;
+            double boyum = 1.75;
+            bool evliMi = false;
+            char ilkHarf = 'A';
+            decimal maas = 5000.50m;
+            DateTime dogumTarihi = new DateTime(2003, 5, 15);
+            Console.WriteLine("İsim: " + isim);
+            Console.WriteLine("Soyisim: " + soyisim);
+            Console.WriteLine("Yaş: " + yasim);
+
+        }
+    }
+}
